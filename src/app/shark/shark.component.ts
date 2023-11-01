cad5e38b6376726d5d5bec105e97ae8d45488c61
brahim-slimani <slimani.ibr@gmail.com>
2023-11-01 14:25:39 +0100

    Add filter cmp

--- src/app/shark/shark.component.ts
+++ src/app/shark/shark.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-shark',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      shark works!
-    </p>
-  `,
-  styleUrls: ['./shark.component.scss']
+  templateUrl: './shark.component.html',
+  styleUrls: ['./shark.component.scss'],
 })
 export class SharkComponent {
-
+  onFilter() {
+    alert("salam");
+  }
 }
