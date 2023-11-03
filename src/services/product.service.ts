bbacbb8c2491877ed9a08e9952642d5d1bec1e84
brahim-slimani <slimani.ibr@gmail.com>
2023-11-03 14:47:06 +0100

    Add product service, fetching http api

---
+++ src/services/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Product } from 'src/interfaces/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  async getProducts(): Promise<any> {
+    const data = await fetch(`${environment.apiUrl}/products`);
+    return data.json();
+  }
+}
