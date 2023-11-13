f585c4e0c7e54b2f604063d9b2788e6ceb067089
brahim-slimani <slimani.ibr@gmail.com>
2023-11-13 14:07:52 +0100

    Add logout action

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { JWTWorkerService } from 'src/app/services/jwt-worker.service';
 
 @Component({
   selector: 'app-header',
@@ -11,4 +12,9 @@ import { RouterModule } from '@angular/router';
 })
 export class HeaderComponent {
 
+  constructor(private jwtWorker: JWTWorkerService) {}
+
+  logout() {
+    this.jwtWorker.deleteToken();
+  }
 }
