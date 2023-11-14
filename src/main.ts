8f795c50507716d1ef590b605798413d53878de4
brahim-slimani <slimani.ibr@gmail.com>
2023-11-14 14:29:42 +0100

    Configure base href option

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/modules/app-routing.module';
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { CustomHttpInterceptor } from './app/modules/http-interceptor';
 
