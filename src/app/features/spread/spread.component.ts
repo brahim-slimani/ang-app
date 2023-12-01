54caa36f6f16baebfa5889b8bccb41facdddd896
brahim-slimani <slimani.ibr@gmail.com>
2023-12-01 09:40:45 +0100

    Add mat-table cmp

--- src/app/features/spread/spread.component.ts
+++ src/app/features/spread/spread.component.ts
@@ -2,15 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/interfaces/product';
 
 
 @Component({
   selector: 'app-spread',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatTableModule],
   templateUrl: './spread.component.html',
   styleUrls: ['./spread.component.scss']
 })
 export class SpreadComponent {
+
+  products: Product[] = [];
+  displayedColumns: string[] = ["id", "title", "brand", "price", "category", "stock", "discountPercentage", "rating"]
+  //displayedColumns: string[] = ["title"]
+
+  constructor(private productService: ProductService) {
+    productService.getProducts().subscribe(data => {
+      this.products = data.products;
+    })
+  }
 
 }
