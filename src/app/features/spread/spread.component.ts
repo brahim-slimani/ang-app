1c65cc3ccfacd78f335a359487cb4ea483da79ba
brahim-slimani <slimani.ibr@gmail.com>
2023-12-01 10:40:41 +0100

    Add pagination option

--- src/app/features/spread/spread.component.ts
+++ src/app/features/spread/spread.component.ts
@@ -1,29 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { ProductService } from 'src/app/services/product.service';
-import { Product } from 'src/app/interfaces/product';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 
 
 @Component({
   selector: 'app-spread',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatButtonModule, MatTableModule],
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatTableModule, MatPaginatorModule],
   templateUrl: './spread.component.html',
   styleUrls: ['./spread.component.scss']
 })
 export class SpreadComponent {
 
-  products: Product[] = [];
+  dataLength = 0;
+  productDataSource = new MatTableDataSource([]);
   displayedColumns: string[] = ["id", "title", "brand", "price", "category", "stock", "discountPercentage", "rating"]
-  //displayedColumns: string[] = ["title"]
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private productService: ProductService) {
     productService.getProducts().subscribe(data => {
-      this.products = data.products;
-    })
+      this.productDataSource = new MatTableDataSource(data.products);
+      this.dataLength = data.products.length;
+      this.productDataSource.paginator = this.paginator;
+    });
   }
 
 }
