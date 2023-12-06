09e48ff755f3ab52ae5632e2ab013293ef36c54f
brahim-slimani <slimani.ibr@gmail.com>
2023-12-06 15:14:15 +0100

    Externalize nav items to const res

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,1 +1,15 @@
 export const REQUIRED_INPUT_MESSAGE = "This field is required."
+export const NAV_ITEMS = [
+    {
+        label: "Home",
+        path: "/home"
+    },
+    {
+        label: "Spread",
+        path: "/spread"
+    },
+    {
+        label: "Create Product",
+        path: "/new-product"
+    }
+]
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { JWTWorkerService } from 'src/app/services/jwt-worker.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { NAV_ITEMS } from '../constants';
 
 @Component({ selector: 'app-header', standalone: true, imports: [CommonModule, RouterModule], templateUrl: './header.component.html', styleUrls: ['./header.component.scss'] })
 export class HeaderComponent {
 
+  navItems: Array<any> = NAV_ITEMS
   constructor(public authService: AuthService) { }
 
 }
